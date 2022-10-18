841b5b60d7c917877e581789be8b060e7d14d3f9
Aidan Barrett <aidbarrett94@gmail.com>
2022-10-18 17:00:45 +0100

    game logic done

--- app.js
+++ app.js
@@ -1,49 +1,97 @@
 const gameTile = document.querySelectorAll(".game-tile");
 const startBtn = document.querySelector(".start-btn");
+const winBox = document.querySelector(".win-box");
+const choiceBox = document.querySelector(".choice-box");
+const pointsBox = document.querySelector(".points");
+const timerBox = document.querySelector(".timer-box");
+const colors = ["blue", "red", "green", "yellow", "pink", "black"];
 const tileArray = Array.from(gameTile);
-console.log(tileArray);
-choice1 = "blue";
-Choice2 = "red";
-choice3 = "green";
-choice4 = "yellow";
-choice5 = "pink";
-gameOverChoice = "black";
+
+gameSpeedChoice = "black";
 let gameplay = false;
+let points = 0;
+let timeLeft = 10;
+let countdown;
 
 startBtn.addEventListener("click", (e) => {
   startBtn.innerText = "||";
   if (gameplay === true) {
-    gameplay = false;
-    startBtn.innerText = "START";
+    gameOver();
   } else {
     gameplay = true;
+    points = 0;
+    pointsBox.innerHTML = points;
+    countdown = setInterval(function () {
+      --timeLeft;
+      timerBox.innerText = timeLeft;
+      if (timeLeft === 0) {
+        gameOver();
+      }
+    }, 1000);
+
     startGame();
   }
 });
 
 function randomColorFunction() {
-  const colors = ["blue", "red", "green", "yellow", "pink", "black"];
   const randomcolor = colors[Math.floor(Math.random() * colors.length)];
+
   return randomcolor;
 }
-
+let time = 4000;
 function startGame(e) {
   if (gameplay === true) {
     tileArray.forEach((element) => {
       element.style.background = randomColorFunction();
+
+      choiceBox.innerText = colors[Math.floor(Math.random() * colors.length)];
+      choiceBox.style.background = choiceBox.innerText;
     });
-    setTimeout(startGame, 1500);
-    gameLogic();
+
+    setTimeout(startGame, time);
   }
+  gameLogic();
 }
 
 function gameLogic() {
   tileArray.forEach((element) => {
     element.addEventListener("click", (e) => {
-      console.log("click");
-      if (e.target.style.background === choice1) {
-        console.log("yeah baby");
+      if (e.target.style.background === choiceBox.innerText) {
+        winBox.innerText = "Yeah!";
+        points++;
+        pointsBox.innerText = `points:${points}`;
+        console.log(points);
+        console.log("click");
+        if (points >= 10) {
+          gameWin();
+        }
+      } else if (e.target.style.background === gameSpeedChoice) {
+        winBox.innerText = "Speeding Up!";
+        setTimeout(startGame, time / 2);
+      } else {
+        gameOver();
       }
     });
   });
+}
+
+function gameOver() {
+  clearInterval(countdown);
+  points = 0;
+  gameplay = false;
+  winBox.innerText = "Try Again";
+  startBtn.innerText = "START";
+
+  timeLeft = 10;
+}
+
+function gameWin() {
+  clearInterval(countdown);
+  points = 0;
+  gameplay = false;
+
+  startBtn.innerText = "START";
+
+  timeLeft = 10;
+  winBox.innerText = "WINNER";
 }
