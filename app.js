4121be0423cddbb7b8ac3597a22236754542fc05
Aidan Barrett <aidbarrett94@gmail.com>
2022-10-21 17:04:01 +0100

    canvas background added

--- app.js
+++ app.js
@@ -23,8 +23,82 @@ let points = 0;
 let timeLeft = 15;
 let countdown;
 
+const canvas = document.querySelector("canvas");
+const c = canvas.getContext("2d");
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+let mouseX;
+let mouseY;
+
+window.addEventListener("mousemove", (e) => {
+  mouseX = e.x;
+  mouseY = e.y;
+});
+window.addEventListener("resize", (e) => {
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight;
+});
+class Circle {
+  constructor(x, y, dx, dy, radius) {
+    this.x = x;
+    this.y = y;
+    this.dx = dx;
+    this.dy = dy;
+    this.radius = radius;
+  }
+  draw() {
+    c.beginPath();
+    c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
+    c.fillStyle = "rgba(255,255,255,0.3)";
+    c.fill();
+  }
+  update() {
+    if (this.x + this.radius > innerWidth || this.x - this.radius < 0) {
+      this.dx = -this.dx;
+    }
+    if (this.y + this.radius > innerHeight || this.y - this.radius < 0) {
+      this.dy = -this.dy;
+    }
+    this.x += this.dx;
+    this.y += this.dy;
+
+    if (
+      mouseX - this.x < 50 &&
+      mouseX - this.x > -50 &&
+      mouseY - this.y < 50 &&
+      mouseY - this.y > -50
+    ) {
+      this.draw();
+      c.fillStyle = "rgba(255,255,255,1)";
+      c.fill();
+    }
+
+    this.draw();
+  }
+}
+const circleArray = [];
+for (let i = 0; i < 200; i++) {
+  let radius = 2;
+  let y = Math.random() * (innerHeight - radius * 2) + radius;
+  let x = Math.random() * (innerWidth - radius * 2) + radius;
+  let dx = (Math.random() - 0.5) * 2;
+
+  let dy = (Math.random() - 0.5) * 2;
+  circleArray.push(new Circle(x, y, dx, dy, radius));
+}
+
+function animate() {
+  requestAnimationFrame(animate);
+  c.clearRect(0, 0, innerWidth, innerHeight);
+
+  for (let i = 0; i < circleArray.length; i++) {
+    circleArray[i].update();
+  }
+}
+
+animate();
+
 rulesBtn.addEventListener("click", (e) => {
-  console.log("copy");
   rules.classList.add("toggle");
 });
 
@@ -96,11 +170,9 @@ function gameLogic() {
 function gameOver() {
   losegameAudio.play();
   clearInterval(countdown);
-
   gameplay = false;
   winBox.innerText = "Try Again";
   startBtn.innerText = "START";
-
   timeLeft = 15;
   pointsBox.innerText = `Total: ${points}`;
   points = 0;
@@ -111,9 +183,7 @@ function gameWin() {
   clearInterval(countdown);
   points = 0;
   gameplay = false;
-
   startBtn.innerText = "START";
-
   timeLeft = 15;
   winBox.innerText = "WINNER!";
 }
